4ada86988b64ae95960abcb21055378b0f33a03a
Ayush Patwal <137012714+ayushpatwal011@users.noreply.github.com>
2026-04-19 23:01:10 +0530

    Update user.route.js

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 const userRouter =express.Router();
 import { isAuth } from "../middlewares/isAuth.js";
-import { getCurrentUser } from "../controllers/User.controller.js";
+import { getCurrentUser } from "../controllers/user.controller.js";
 
 userRouter.get('/me',isAuth, getCurrentUser);
 
