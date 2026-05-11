c57fe080b2447151cffbeaeca87eff47a0a80c52
Ayush Patwal <137012714+ayushpatwal011@users.noreply.github.com>
2026-05-11 09:57:45 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,9 +11,9 @@ import Pricing from './pages/Pricing'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-// export const SERVER_URL = 'https://web-builder-xlqe.onrender.com'
+export const SERVER_URL = 'https://web-builder-xlqe.onrender.com'
 // export const SERVER_URL = 'http://localhost:8000'
-export const SERVER_URL = import.meta.env.VITE_SERVER_URL
+// export const SERVER_URL = import.meta.env.VITE_SERVER_URL
 
 const App = () => {
   useGetCurrentUser()
